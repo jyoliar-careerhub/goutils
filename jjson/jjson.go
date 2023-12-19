f85974ffd7016f135501b98a3eac8c7aa8a1cf33
jyo <jyo.liar@gmail.com>
2023-12-19 13:52:14 +0900

    jjson 패키지 UnmarshalReader 추가

--- jjson/jjson.go
+++ jjson/jjson.go
@@ -1,9 +1,18 @@
 package jjson
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"io"
+)
 
 func Unmarshal[T any](data []byte) (*T, error) {
 	value := new(T)
 	err := json.Unmarshal(data, value)
+	return value, err
+}
+
+func UnmarshalReader[T any](reader io.Reader) (*T, error) {
+	value := new(T)
+	err := json.NewDecoder(reader).Decode(value)
 	return value, err
 }
