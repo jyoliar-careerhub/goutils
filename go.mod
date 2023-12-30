ccbde393e9147a5befbeeb35138d5293697979e7
jyo <jyo.liar@gmail.com>
2023-12-30 20:08:02 +0900

    ptr 패키지 벤치마크 테스트 추가

--- go.mod
+++ go.mod
@@ -3,8 +3,10 @@ module github.com/jae2274/goutils
 go 1.21.4
 
 require (
+	github.com/aclements/go-moremath v0.0.0-20210112150236-f10218a38794 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/stretchr/testify v1.8.4 // indirect
+	golang.org/x/perf v0.0.0-20231127181059-b53752263861 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- ptr/ptr_bench_test.go
+++ ptr/ptr_bench_test.go
@@ -1,28 +1,47 @@
 package ptr
 
 import (
+	"fmt"
+	"os"
 	"testing"
 )
 
 func BenchmarkIsNil(b *testing.B) {
+	old := os.Stdout
+
+	// 표준 출력을 /dev/null로 변경
+	file, _ := os.Create("/dev/null")
+	os.Stdout = file
+
+	b.Run("Just check nil via if", func(b *testing.B) {
+		var sampleErr *SampleError = nil
+		for i := 0; i < b.N; i++ {
+			fmt.Print(sampleErr == nil)
+		}
+	})
+
 	b.Run("basic error", func(b *testing.B) {
 		var _ *SampleError = nil
 		for i := 0; i < b.N; i++ {
-			IsNil(nil)
+			fmt.Print(IsNil(nil))
+
 		}
 	})
 
 	b.Run("custom pointer error", func(b *testing.B) {
 		var sampleErr *SampleError = nil
 		for i := 0; i < b.N; i++ {
-			IsNil(sampleErr)
+			fmt.Print(IsNil(sampleErr))
 		}
 	})
 
 	b.Run("Not nil", func(b *testing.B) {
 		var sampleErr *SampleError = &SampleError{}
 		for i := 0; i < b.N; i++ {
-			IsNil(&sampleErr)
+			fmt.Print(IsNil(&sampleErr))
 		}
 	})
+
+	// 표준 출력 원복
+	os.Stdout = old
 }
