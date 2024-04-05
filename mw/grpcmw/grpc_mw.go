dcc5001bdb01dbe2c0e9858165c11a6ab422876e
jyo <jyo.liar@gmail.com>
2024-04-05 14:13:59 +0900

    grpc streamMW 추가

--- mw/grpcmw/grpc_mw.go
+++ mw/grpcmw/grpc_mw.go
@@ -9,7 +9,7 @@ import (
 	"google.golang.org/grpc"
 )
 
-func SetTraceIdMW() grpc.UnaryServerInterceptor {
+func SetTraceIdUnaryMW() grpc.UnaryServerInterceptor {
 	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (_ interface{}, err error) {
 		ctx = mw.SetIfNotExists(ctx, mw.CtxKeyTraceID, uuid.New().String())
 
@@ -18,12 +18,45 @@ func SetTraceIdMW() grpc.UnaryServerInterceptor {
 	}
 }
 
-func LogErrMW() grpc.UnaryServerInterceptor {
+func LogErrUnaryMW() grpc.UnaryServerInterceptor {
 	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (_ interface{}, err error) {
 		resp, err := handler(ctx, req)
 		if err != nil {
 			llog.LogErr(ctx, err)
 		}
 		return resp, err
+	}
+}
+
+// wrappedStream 구조체는 grpc.ServerStream을 래핑하고,
+// 추가 컨텍스트를 포함합니다.
+type wrappedStream struct {
+	grpc.ServerStream
+	ctx context.Context
+}
+
+// Context 메서드는 원래 스트림의 컨텍스트 대신
+// 래핑된 스트림의 컨텍스트를 반환합니다.
+func (w *wrappedStream) Context() context.Context {
+	return w.ctx
+}
+
+func SetTraceIdStreamMW() grpc.StreamServerInterceptor {
+	return func(srv interface{}, stream grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler) error {
+		ctx := stream.Context()
+		newCtx := mw.SetIfNotExists(ctx, mw.CtxKeyTraceID, uuid.New().String())
+
+		wrapped := &wrappedStream{stream, newCtx}
+		return handler(srv, wrapped)
+	}
+}
+
+func LogErrStreamMW() grpc.StreamServerInterceptor {
+	return func(srv interface{}, stream grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler) error {
+		err := handler(srv, stream)
+		if err != nil {
+			llog.LogErr(stream.Context(), err)
+		}
+		return err
 	}
 }
